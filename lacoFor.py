ba732270bb356def75351fc025247da3f2b23679
Ewaldo <ewaldo2016@gmail.com>
2023-10-21 15:58:14 -0300

    Códigos python para resolução dos exercícios de laços de repetiçao

---
+++ lacoFor.py
@@ -0,0 +1,11 @@
+#Declaração das variáveis
+andar = 0
+
+#Estrutura de repetição for
+for i in range(20):
+    andar = andar + 1
+    if(andar == 13):
+        continue
+    print(andar)
+
+
---
+++ lacoWhile.py
@@ -0,0 +1,8 @@
+# Declaração das variáveis.
+andar = 0
+# Laço de repetição While
+while andar < 20:
+  andar = andar + 1
+  if (andar == 13):
+        continue
+  print(andar)
